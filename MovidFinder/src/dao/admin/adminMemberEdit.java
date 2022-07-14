05a74ab96047497544b207b82379e3d91be7e87e
dongry-lee <89010847+dongry-lee@users.noreply.github.com>
2022-07-14 18:17:45 +0900

    intro

--- MovidFinder/src/dao/admin/adminMemberEdit.java
+++ MovidFinder/src/dao/admin/adminMemberEdit.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
-
+//안녕하세욤
 public class adminMemberEdit {
     protected adminMemberEdit() {
         try{
